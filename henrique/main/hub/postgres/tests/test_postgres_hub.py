34b20e073eaca030c1ecd71e2fa0ff7003f8fc51
Moonyoung Kang <yerihyo@gmail.com>
2019-12-26 14:27:39 -0800

    skip postgres test

--- henrique/main/hub/postgres/tests/test_postgres_hub.py
+++ henrique/main/hub/postgres/tests/test_postgres_hub.py
@@ -1,6 +1,8 @@
 import logging
 from unittest import TestCase
 
+import pytest
+
 from henrique.main.hub.logger.logger import HenriqueLogger
 from henrique.main.hub.postgres.postgres_hub import PostgresHub
 
@@ -10,6 +12,7 @@ class PostgresHubTest(TestCase):
     def setUpClass(cls):
         HenriqueLogger.attach_stderr2loggers()
 
+    @pytest.mark.skip(reason="postgres test unnecessary")
     def test_01(self):
         logger = HenriqueLogger.func_level2logger(self.test_01, logging.DEBUG)
 
