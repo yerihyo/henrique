08cd446e1c51a9162a7e4e4e73557f37a05f3ca6
Moonyoung Kang <yerihyo@gmail.com>
2020-03-28 11:45:43 -0700

    small change

--- scripts/direnv/load.bash
+++ scripts/direnv/load.bash
@@ -5,15 +5,21 @@ FILE_PATH=$(readlink -f $ARG0)
 FILE_DIR=$(dirname $FILE_PATH)
 FILE_NAME=$(basename $FILE_PATH)
 
-SCRIPTS_DIR=$(dirname $FILE_DIR)
-LPASS_DIR=$SCRIPTS_DIR/lpass
-
 errcho(){ >&2 echo $@; }
+func_count2reduce(){
+    local v="${1?missing}"; local cmd="${2?missing}"; local n=${3?missing};
+    for ((i=0;i<$n;i++)); do v=$($cmd $v) ; done; echo "$v"
+}
 
-FOXYLIB_DIR=${FOXYLIB_DIR?"FOXYLIB_DIR missing"}
 
-errcho "[$FILE_NAME] START"
-listfile_filepath=$($LPASS_DIR/tmplt2filelist.bash)
-$FOXYLIB_DIR/scripts/direnv/load.bash "$listfile_filepath" "$@"
+main(){
+    $FOXYLIB_DIR/foxylib/tools/direnv/tmplt2load.bash "$REPO_DIR/scripts/lpass/filelist.tmplt.list"
+}
 
-errcho "[$FILE_NAME] END"
+export REPO_DIR=$(func_count2reduce $FILE_DIR dirname 2)
+LPASS_DIR=$REPO_DIR/scripts/lpass
+FOXYLIB_DIR=${FOXYLIB_DIR?'missing $FOXYLIB_DIR'}
+
+echo "[$FILE_NAME] START"
+main
+echo "[$FILE_NAME] END"
