3ef7a5cfe59ff0a98cf5dd04018dd5ad7245a671
Moonyoung Kang <yerihyo@gmail.com>
2020-03-30 16:30:25 -0700

    added culture_port

---
+++ henrique/main/skill/culture/culture_port/culture_port_response.py
@@ -0,0 +1,27 @@
+import os
+
+from foxylib.tools.jinja2.jinja2_tool import Jinja2Renderer
+from foxylib.tools.string.string_tool import str2strip
+from henrique.main.entity.culture.culture import Culture
+from henrique.main.entity.port.port_entity import Port
+
+FILE_PATH = os.path.realpath(__file__)
+FILE_DIR = os.path.dirname(FILE_PATH)
+
+
+class CulturePortResponse:
+    @classmethod
+    def codename_lang2response(cls, port_codename, lang):
+
+        port = Port.codename2port(port_codename)
+        culture = Culture.codename2culture(Port.port2culture(port))
+
+        filepath = os.path.join(FILE_DIR, "tmplt.{}.part.txt".format(lang))
+
+        data = {"culture_name": Culture.culture_lang2name(culture, lang),
+                "port_name": Port.port_lang2name(port, lang)
+                }
+        text_out = str2strip(Jinja2Renderer.textfile2text(filepath, data))
+
+        return text_out
+
