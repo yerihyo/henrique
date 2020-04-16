ce08f55bc476457219556569ba37b0b22d41f829
Moonyoung Kang <yerihyo@gmail.com>
2020-04-15 22:12:14 -0700

    added ?help

--- henrique/main/skill/help/tests/test_help_skill_description.py
+++ henrique/main/skill/help/tests/test_help_skill_description.py
@@ -1,16 +1,12 @@
-from pprint import pprint
 from unittest import TestCase
 
-from henrique.main.skill.port.port_skill import PortSkill
-from henrique.main.skill.port.port_skill_description import PortSkillDescription
-from khala.document.chatroom.chatroom import KakaotalkUWOChatroom
-from khala.document.packet.packet import KhalaPacket
+from henrique.main.skill.help.help_skill_description import HelpSkillDescription
 
 
 class TestHelpSkillDescription(TestCase):
     def test_01(self):
 
-        description = PortSkillDescription.lang2text("ko")
+        description = HelpSkillDescription.lang2text("ko")
 
         # pprint({"description":description})
         self.assertTrue(description.startswith("?도움말 명령어"))
--- henrique/main/skill/port/tests/test_port_skill.py
+++ henrique/main/skill/port/tests/test_port_skill.py
@@ -26,7 +26,7 @@ class TestPortSkill(TestCase):
                   }
 
         hyp = PortSkill.packet2response(packet)
-        ref = '[이베리아] 항구 - 라스팔마스, 파루, 카사블랑카, 히혼, 팔마, 마데이라, 비아나두카스텔루, 리스본, 사그레스, 빌바오, 세비야, 바르셀로나, 포르투, 말라가, 세우타, 몽펠리에, 발렌시아'
+        ref = '[이베리아] 항구 - 라스팔마스, 카사블랑카, 비아나두카스텔루, 파루, 마데이라, 팔마, 히혼, 리스본, 세비야, 빌바오, 사그레스, 바르셀로나, 세우타, 포르투, 말라가, 몽펠리에, 발렌시아'
 
         # pprint({"hyp":hyp})
         self.assertEqual(hyp, ref)
