f12057da9559025c6dacfc0a65e15d03855db258
Moonyoung Kang <yerihyo@gmail.com>
2020-05-28 12:36:12 -0700

    (HRQ-6) slack message

--- henrique/main/singleton/socialmedia/slack/henrique_slack.py
+++ henrique/main/singleton/socialmedia/slack/henrique_slack.py
@@ -10,5 +10,5 @@ class HenriqueSlackbot:
         logger = HenriqueLogger.func_level2logger(cls.xoxb_token, logging.DEBUG)
 
         token = HenriqueEnv.key2value("SLACK_HENRIQUE_BOT_USER_OAUTH_ACCESS_TOKEN")
-        # logger.debug({"token": token})
+        logger.debug({"bool(token)": bool(token)})
         return token
